b3adfdabb812f789b3e568e2d8ac4a1ca7d026da
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-10-31 15:59:37 +0100

    Add files via upload

---
+++ OperationType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculatorWindows
+{
+    /// <summary>
+    /// A type of Operation the calculator can perform .
+    /// </summary>
+    public enum OperationType
+    { 
+        /// <summary>
+        /// Add Two values together .
+        /// </summary>
+        Add,
+        /// <summary>
+        /// Takes one value from another .
+        /// </summary>
+        Minus,
+        /// <summary>
+        /// Divides one number by another .
+        /// </summary>
+        Divide,
+        /// <summary>
+        /// Multiply two numbers Together .
+        /// </summary>
+        Multiply
+    }
+}
---
+++ TheOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculatorWindows
+{
+    /// <summary>
+    /// Holds Information about a calculator Operation .
+    /// </summary>
+   public class TheOperation
+    {
+        /// <summary>
+        /// The Left side of operation
+        /// </summary>
+        public string LeftSide { get; set; }
+
+        /// <summary>
+        /// The Right side of operation 
+        /// </summary>
+        public string RightSide { get; set; }
+
+    }
+}
